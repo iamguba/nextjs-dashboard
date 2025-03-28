45935a00ad4faf61764ca40f8f7e59b12eeec30f
guba <gubacoolest@gmail.com>
2025-03-28 16:59:39 +0200

    add auth etc.

---
+++ auth.ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import postgres from "postgres";
+import { User } from "./app/lib/definitions";
+import bcrypt from "bcryptjs";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const CredentialsSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = CredentialsSchema.safeParse(credentials);
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (user && (await bcrypt.compare(password, user.password))) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
